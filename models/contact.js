378dd5c36bb7692ac0040a07ac634a752c57f682
Paras Kaushik <paraskaushik12@gmail.com>
2021-11-20 17:11:37 +0530

    added css

--- models/contact.js
+++ models/contact.js
@@ -1,19 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
 
-    name: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: String,
-        required: true
-    }
-
-})
-
-
-const Contact = mongoose.model('Contact', contactSchema);
-
+const Contact = mongoose.model("Contact", contactSchema);
+// Contact is name of collection
 module.exports = Contact;
